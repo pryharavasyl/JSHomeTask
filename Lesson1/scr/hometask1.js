1d85f51ed5b609cde0a74627fe68be12cf387e04
pryharavasyl <31851835+pryharavasyl@users.noreply.github.com>
2019-01-12 23:52:05 +0300

    Update hometask1.js

--- Lesson1/scr/hometask1.js
+++ Lesson1/scr/hometask1.js
@@ -3,12 +3,24 @@ function name (firstname, lastname) {
     if (lastname == undefined) {
         return 'Hello my name is ' + firstname;
     }
-    else {
-    return 'Hello, my name is ' + firstname + lastname;
+    else if (firstname == undefined) {
+        return "Hello, my lastname is " + lastname;
     }
-}
+    else if (name == undefined) {
+        return '';
+    }
+    else {
+        return 'Hello, my name is ' + firstname + lastname;
+    }
+   
+    }
+
 
 var person1 = name ('vasyl', 'pryhara');
 console.log (person1);
 var person2 = name ('bohdan');
 console.log (person2);
+var person3 = name (null ,'Dorosh');
+console.log (person3);
+var person4 = name ();
+console.log (person4);
