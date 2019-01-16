08633e90bacfd0a9373d526d94acdcc2b1ea17b6
Bohdan Dorosh <doroshbohdan1@gmail.com>
2019-01-16 20:20:45 +0200

    review task1

--- Lesson1/scr/hometask1.js
+++ Lesson1/scr/hometask1.js
@@ -1,5 +1,5 @@
 
-function name (firstname, lastname) {
+function name(firstname, lastname) {
     if (lastname == undefined) {
         return 'Hello my name is ' + firstname;
     }
@@ -12,15 +12,59 @@ function name (firstname, lastname) {
     else {
         return 'Hello, my name is ' + firstname + lastname;
     }
-   
+
+}
+
+
+var person1 = name('vasyl', 'pryhara');
+console.log(person1);
+var person2 = name('bohdan');
+console.log(person2);
+var person3 = name(null, 'Dorosh');
+console.log(person3);
+var person4 = name();
+console.log(person4);
+
+// #1 fix logic
+function name(firstname, lastname) {
+    if (firstname == undefined && lastname == undefined) {
+        return '';
     }
+    if (lastname == undefined) {
+        return 'Hello my name is ' + firstname;
+    }
+    if (firstname == undefined) {
+        return "Hello, my lastname is " + lastname;
+    }
+    return 'Hello, my name is ' + firstname + ' ' + lastname;
+}
 
+// #2 Store common things in variables. Do not hardcode values.
+function name(firstname, lastname) {
+    var firstPart = 'Hello my name is ';
+    if (firstname == undefined && lastname == undefined) {
+        return '';
+    }
+    if (lastname == undefined) {
+        return firstPart + firstname;
+    }
+    if (firstname == undefined) {
+        return firstPart + lastname;
+    }
+    return firstPart + firstname + ' ' + lastname;
+}
 
-var person1 = name ('vasyl', 'pryhara');
-console.log (person1);
-var person2 = name ('bohdan');
-console.log (person2);
-var person3 = name (null ,'Dorosh');
-console.log (person3);
-var person4 = name ();
-console.log (person4);
+// #3 use falsy values feature.
+function name(firstname, lastname) {
+    var firstPart = 'Hello my name is ';
+    if (!firstname && !lastname) {
+        return '';
+    }
+    if (!lastname) {
+        return firstPart + firstname;
+    }
+    if (!firstname) {
+        return firstPart + lastname;
+    }
+    return firstPart + firstname + ' ' + lastname;
+}
