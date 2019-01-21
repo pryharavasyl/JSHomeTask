665429679383f8be06d2ff63b9f6efa7d9c719eb
Bohdan Dorosh <doroshbohdan1@gmail.com>
2019-01-21 20:47:46 +0200

    review task 2

--- Lesson2/scr2/hometask2.js
+++ Lesson2/scr2/hometask2.js
@@ -1,4 +1,4 @@
-function resultQA (a, b, c) {
+function resultQE (a, b, c) {
     var d = b*b - 4 * a * c;
     var firstx = (-b+(Math.pow(d, 0.5)))/(2*a);
     var secondx = (-b-(Math.pow(d, 0.5)))/(2*a);
@@ -7,7 +7,18 @@ function resultQA (a, b, c) {
     emptyarrey.push (firstx);
     emptyarrey.push (secondx);
     console.log(emptyarrey);
-
-
+    return emptyarrey;
 }
-var firstexamples = resultQA (1, 6, 5);
+var firstexamples = resultQE(1, 6, 5);
+
+// #1 implement 3 
+function resultQE (a, b, c) {
+    var d = b*b - 4 * a * c;
+    if (d < 0) {
+        return [NaN, NaN];
+    }
+    var sqrtD = Math.pow(d, 0.5);
+    var firstx = (-b+sqrtD)/(2*a);
+    var secondx = (-b-sqrtD)/(2*a);
+    return [firstx, secondx];
+}
