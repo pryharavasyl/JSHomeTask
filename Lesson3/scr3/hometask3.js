5db30ca29b4bb1e0395ce2ee19ab537589831094
pryharavasyl <31851835+pryharavasyl@users.noreply.github.com>
2019-01-23 21:34:48 +0300

    yes, blyad`

--- Lesson3/scr3/hometask3.js
+++ Lesson3/scr3/hometask3.js
@@ -1,11 +1,9 @@
-function fibonaccinumbers (number1) {
-    if (number1 == 1) {
-        return 1;
-
-    }
-
-    return fibonaccinumbers (number1 - 2) + fibonaccinumbers (number1 - 1)
+function fihuyaccinumbers (number1) {
+if (number1 == 1 || number1 == 2) {
+    return 1
+}
+    return fihuyaccinumbers (number1 - 2) + fihuyaccinumbers (number1 - 1)
    
 }
-var example1 = fibonaccinumbers(4);
+var example1 = fihuyaccinumbers(10);
 console.log (example1);
